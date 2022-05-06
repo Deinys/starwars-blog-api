b95c856741f93b9bd714c7258fd5dee87f20da4b
Deinys <dpulido200@hotmail.com>
2022-05-06 11:19:31 -0400

    starwars beginning

--- src/main.py
+++ src/main.py
@@ -8,13 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from flask_jwt_extend import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from models import db, User, Favorites, Pleople, Plantet
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.environ.get('FLASK_APP_KEY')
+jwt =JWTManager(app)
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -29,6 +32,26 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+URL_BASE = "https://www.swapi.dev/api/"
+@app.route('/people', methods=['POST'])
+def handle_characters():
+    response = request.get(f"{URL_BASE}/people")
+    response = response.json()
+    all_results = []
+
+    for result in response['results']:
+        detail = request.get(result['url'])
+        detail = detail.json()
+        all_results.append(detail)
+
+    instance = []
+
+    for people in all_results:
+        instance = people.create(people)
+        instance.append(instance)
+    return jsonify(list(map(lambda inst: inst.serialize(), instance))), 200
+
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
--- src/models.py
+++ src/models.py
@@ -17,3 +17,60 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Favorites(db.Model):
+    id = db.Column(db.integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForegnKey("user.id"), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForegnKey("planet.id"), nullable=False)
+    people_id = db.Column(db.Integer, db.ForegnKey("people.id"), nullable=False)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.username
+
+    def serialize(self):
+        return {
+            "user_id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "people_id": self.people_id,
+            # do not serialize the password, its a security breach
+        }
+class Planet(db.Model):
+    id = db.Column(db.integer, primary_key = True)
+    name = db.Column(db.String(250))
+    population = db.Column(db.String(250))
+    terrain = db.Column(db.String(250))
+    favorites =db.relationship("Favorites")
+
+    def __repr__(self):
+        return '<Planet %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "terrain": self.terrain,
+            # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    id = db.Column(db.integer, primary_key = True)
+    name = db.Column(db.String(250))
+    gender = db.Column(db.String(250))
+    hair_color = db.Column(db.String(100))
+    eyes_color = db.Column(db.String(100))
+    favorites = relationship("favorites")
+
+    def __repr__(self):
+        return '<People %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "eyes_color": self.eyes_color,
+            # do not serialize the password, its a security breach
+        }
